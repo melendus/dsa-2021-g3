aa508cb6f63f600e2d2014cf71163b2418160240
Sandu Mihai <mundarini0802@gmail.com>
2021-03-09 20:48:23 +0200

    Lab02 Sandu Mihai

---
+++ 30413/Mihai Alexandru Sandu/Lab03/extra/main.c
@@ -0,0 +1,167 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*  *******************************************************
+
+    I wrote those functions and structures just to exercise
+
+    ******************************************************** */
+
+
+ typedef struct node
+    {
+        int value;
+        struct node *next;
+    }NodeT;
+
+typedef struct SLL
+{
+    int count;
+    NodeT *first;
+    NodeT *last;
+}SLListT;
+
+SLListT *createEmptySLList()
+{
+    SLListT *list = (SLListT*)malloc(sizeof(SLListT));
+    if (list)
+    {
+        list->count = 0;
+        list->first = list->last = NULL;
+    }
+    return list;
+}
+
+NodeT *createNode(int value)
+{
+    NodeT *p = (NodeT*)malloc(sizeof(NodeT));
+    if (p)
+    {
+        p->next = NULL;
+        p->value = value;
+    }
+    return p;
+}
+
+void AddFirst(SLListT *list, NodeT *p)
+{
+    if ( list )
+    {
+        if (list->first == NULL && list->last == NULL)
+        {
+            list->first = list->last = p;
+        }
+        else
+        {
+            p->next = list->first;
+            list->first = p;
+        }
+        list->count++;
+    }
+}
+
+// Function to find the intersection of two lists
+// input : 2 SLLists
+// output: the first node of the intersection
+
+NodeT *FindIntersection( SLListT *list1, SLListT *list2)
+{
+    NodeT *p1,*p2;
+    if ( list1 && list2 )
+    {
+        for ( p1 = list1->first; p1 != NULL; p1 = p1->next )
+            {
+                for ( p2 = list2->first; p2 != NULL; p2 = p2->next )
+                {
+                    if ( p1->value == p2->value )
+                        return p2;
+                }
+            }
+                if ( p1 == NULL && p2 == NULL) return NULL;
+    }
+    else return NULL;
+}
+void SortSLList(SLListT *list)
+{
+    NodeT *p1;
+    NodeT *p2;
+    NodeT *aux;
+
+    for(p1 = list->first; p1 != NULL; p1 = p1->next)
+    {
+        for(p2 = p1->next; p2 != NULL; p2 = p2->next)
+        {
+            if (p1->value == p2->value)
+            {
+                aux = p1;
+                p1 = p2;
+                p2 = aux;
+            }
+        }
+    }
+}
+
+void DeleteDuplicates(SLListT *list)
+{
+    SortSLList(list);
+    NodeT *p1 = list->first;
+    NodeT *p2;
+    while (p1 != NULL)
+    {
+        p2 = p1->next;
+        if (p2 != NULL)
+        {
+            if (p1->value == p2->value)
+            {
+                if (p2->next == NULL)
+                {
+                    p1->next = NULL;
+                    free(p2);
+                }
+                else
+                {
+                    p1->next = p2->next;
+                    free(p2);
+                }
+            }
+        }
+        p1 = p1->next;
+    }
+}
+void show(SLListT *list)
+{
+    NodeT *p = list->first;
+    while (p != NULL)
+    {
+        printf("%d ",p->value);
+        p = p->next;
+    }
+}
+
+int main()
+{
+    SLListT *list1, *list2;
+    list1 = createEmptySLList();
+    list2 = createEmptySLList();
+    NodeT *p1,*p2,*p3,*p4,*p5,*p6,*nodeIntersection;   // I was lazy so I did this
+    p1 = createNode(5);
+    p2 = createNode(2);
+    p3 = createNode(9);
+    p4 = createNode(4);
+    p5 = createNode(11);
+    p6 = createNode(9);
+    AddFirst(list1,p1);
+    AddFirst(list1,p2);
+    AddFirst(list1,p3);
+    AddFirst(list1,p4);
+    AddFirst(list1,p5);
+    AddFirst(list1,p6);
+    //nodeIntersection = FindIntersection(list1,list2);
+    DeleteDuplicates(list1);
+    show(list1);
+
+    //printf("%d",nodeIntersection->value);
+
+    return 0;
+}
